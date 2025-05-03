0a5fa9e1bb426a5a9bcd58eafd49c2bcf28fe146
diego-villamil <diegosanjuan9000@gmail.com>
2025-05-03 18:33:50 -0500

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        hiolakla
         <main>{children}</main>
       </body>
       <Toaster />
