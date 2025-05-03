049a185bec11f2867300ccf262cc221b4e51f820
alvar.dev <alvaropedrozo07@gmail.com>
2025-05-03 17:21:50 -0500

    feat: add amplify configuration and remove unused middleware

--- app/(admin)/dashboard/page.js
+++ app/(admin)/dashboard/page.js
@@ -1,5 +1,9 @@
 "use client"
+import { Amplify } from "aws-amplify";
 import Dashboard from "../../../components/dashboard/dashboard"
+
+import outputs from "@/amplify_outputs.json";
+Amplify.configure(outputs);
 
 export function Home() {
   return (
